3abf874f8c417746f9dd8404f972086bc2c1be26
Muhammad Hamza <muhammad.hamza6415@gmail.com>
2023-12-17 13:43:02 -0500

    inventorydao done

--- src/main/java/com/yearup/dealership/db/InventoryDao.java
+++ src/main/java/com/yearup/dealership/db/InventoryDao.java
@@ -3,6 +3,7 @@ package com.yearup.dealership.db;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class InventoryDao {
@@ -13,10 +14,35 @@ public class InventoryDao {
     }
 
     public void addVehicleToInventory(String vin, int dealershipId) {
-        // TODO: Implement the logic to add a vehicle to the inventory
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(
+                     "INSERT INTO inventory (dealership_id,VIN) VALUES (?, ?)",
+                     PreparedStatement.RETURN_GENERATED_KEYS)) {
+            statement.setInt(1, dealershipId);
+            statement.setString(2, vin);
+            statement.executeUpdate();
+
+
+            try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    int dealership = generatedKeys.getInt(1);
+                    String vehicleid = generatedKeys.getString(2);
+                    System.out.println("Vehicle with vin: "+vehicleid+" added to the dealership with id: "+dealership);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void removeVehicleFromInventory(String vin) {
-        // TODO: Implement the logic to remove a vehicle from the inventory
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(
+                     "DELETE FROM inventory WHERE VIN = ?")) {
+            statement.setString(1, vin);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
